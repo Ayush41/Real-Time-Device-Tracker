801b685db89a9d33f693c2b251d642328c1d5694
ayush41 <anonymousking41681@gmail.com>
2024-12-25 21:12:57 +0530

    market 2 location done

--- main.js
+++ main.js
@@ -11,8 +11,15 @@ const io = socketio(server);
 app.set("view engine","ejs");
 app.use(express.static(path.join(__dirname,"public")));
 
-io.on("Connection",(socket)=>{
-    console.log("a user connected");
+io.on("connection",(socket)=>{
+    console.log("a user connected",socket.id);
+    socket.on("send-location",(data)=>{
+        console.log("Receive Location: ",data);
+        io.emit("receive-location",{id:socket.id,...data}); //spred operator
+    });
+    socket.on("disconnect",()=>{
+        io.emit("user-disconnected",socket.id);
+    })
 });
 
 app.get('/',(req,res)=>{
--- public/js/script.js
+++ public/js/script.js
@@ -43,8 +43,44 @@ if (navigator.geolocation) {
     );
 }
 
-const map = L.map("map").setView([0, 0], 10);
+const map = L.map("map").setView([0, 0], 16);
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution: "OpenStreetMap",
 }).addTo(map);
+
+const markers = {}; //empty object markers
+socket.on("receive-location",(data)=>{
+    console.log("location reveived",data);
+    const {id,latitude,longitude} = data;
+
+    map.setView([latitude,longitude]);
+    if(markers[id]){
+        markers[id].setLatLng([latitude, longitude]);
+    }else{
+        markers[id] = L.marker([latitude,longitude]).addTo(map);
+    }
+});
+
+socket.on("user-disconnected", (id) => {
+    if (markers[id]) {
+        map.removeLayer(markers[id]);
+        delete markers[id];
+    }
+});
+
+
+// socket.on("user-disconnect", (id) => {
+//     if (markers[id]) {
+//         map.removeLayer(markers[id]);
+//         delete markers[id];
+//     }
+// });
+
+
+// socket.on("user-disconnect",(id)=>{
+//     if(markers[id]){
+//         map.removeLayer(markers[id])
+//         delete markers[id];
+//     }
+// });
