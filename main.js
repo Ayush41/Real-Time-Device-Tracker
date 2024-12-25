784596827fac753f6a430be34edb3490a973507c
ayush41 <anonymousking41681@gmail.com>
2024-12-25 19:48:46 +0530

    getting location

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const app = express();
-const https = require("http");
+const http = require("http");
 const path = require('path');
 
 // Setting up socket.io
-const io = require("socket.io");
-
-const server = https.createServer(app);
-
+const socketio = require("socket.io");
+const server = http.createServer(app);
 const io = socketio(server);
 
-
 app.set("view engine","ejs");
-app.set(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname,"public")));
+
+io.on("Connection",(socket)=>{
+    console.log("a user connected");
+});
 
 app.get('/',(req,res)=>{
-    res.send("Server started");
+    res.render("index");
 });
 
 server.listen(3030,()=>{
